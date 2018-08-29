d07be7bfe51b8ea44b496e36b58b2ebfec2b0d3e
xyl666 <492113976@qq.com>
2018-08-29 11:43:32 +0800

    新增输入框限制小数点后两位组件

---
+++ src/components/Items/inputNumber.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-input v-model="valueProp" :placeholder="placeholder" :maxlength="maxLength" @input.native="$emit('input', $event.target.value)">
+    <template slot="append"><slot name="append"></slot></template>
+  </el-input>
+</template>
+<script>
+export default {
+  props: ['value', 'placeholder'],
+  data () {
+    return { valueProp: this.value }
+  },
+  updated () {
+    console.log('gengxin')
+    this.valueProp = this.value
+  },
+  computed: {
+    maxLength () {
+      let idx = this.value ? this.value.indexOf('.') : -4
+      console.log(idx)
+      if (idx === -1) {
+        idx = -4
+      }
+      return idx + 3
+    }
+  }
+}
+</script>
